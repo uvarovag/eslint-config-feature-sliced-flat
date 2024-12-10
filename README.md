af3bbe03a5e1586df3062cb6561a83557585003f
uvarovag <aleanches@gmail.com>
2024-12-10 17:52:06 +0300

    init

---
+++ README.md
@@ -0,0 +1,19 @@
+# @feature-sliced/eslint-config adapter for ESLint 9 Flat Config
+
+A shared ESLint configuration for TypeScript and React projects.
+
+## Installation
+
+To use this configuration in your project, install the required dependencies:
+
+```bash
+npm install --save-dev @uvarovag/eslint-config-feature-sliced-flat eslint@^9.16.0
+```
+
+## Usage: Create a `eslint.config.json` file
+
+```js
+import featureSliced from '@uvarovag/eslint-config-feature-sliced-flat'
+
+export default [...featureSliced, ...projectFlatConfig]
+```
---
+++ eslint.config.js
@@ -0,0 +1,37 @@
+import boundaries from 'eslint-plugin-boundaries'
+import importPlugin from 'eslint-plugin-import'
+import { createRequire } from 'module'
+
+const require = createRequire(import.meta.url)
+
+const importOrder = require('@feature-sliced/eslint-config/rules/import-order')
+const layersSlices = require('@feature-sliced/eslint-config/rules/layers-slices')
+const publicApi = require('@feature-sliced/eslint-config/rules/public-api')
+
+export const importOrderFlat = {
+    files: ['**/*.js', '**/*.ts', '**/*.jsx', '**/*.tsx'],
+    plugins: {
+        import: importPlugin,
+    },
+    rules: importOrder.rules,
+}
+
+export const layersSlicesFlat = {
+    files: ['**/*.js', '**/*.ts', '**/*.jsx', '**/*.tsx'],
+    ...boundaries.configs.recommended,
+    plugins: {
+        boundaries,
+    },
+    settings: layersSlices.settings,
+    rules: layersSlices.rules,
+}
+
+export const publicApiFlat = {
+    files: ['**/*.js', '**/*.ts', '**/*.jsx', '**/*.tsx'],
+    plugins: {
+        import: importPlugin,
+    },
+    rules: publicApi.rules,
+}
+
+export default [importOrderFlat, publicApiFlat, layersSlicesFlat]
